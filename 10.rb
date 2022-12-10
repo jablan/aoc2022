31c6ac496a55805447b803dfa764b645d8227371
Mladen Jablanovic <mladen@jablanovic.com>
2022-12-10 06:36:51 +0100

    day 10

---
+++ 10.rb
@@ -0,0 +1,45 @@
+input = ARGF.each_line.map do |line|
+    line.rstrip
+end
+
+x = 1
+c = 1
+@sum = 0
+@xpos = 1
+
+def click(c, x)
+    # puts "xpos: #{@xpos}, x: #{x}"
+    if @xpos >= x && @xpos < x + 3
+        print '#'
+    else
+        print '.'
+    end
+    @xpos += 1
+    if @xpos > 40
+        puts
+        @xpos = 1
+    end
+
+    if (c+20) % 40 == 0
+        @sum += x * c
+    end
+end
+
+input.each do |line|
+    ci = 0
+    xi = 0
+    case line
+    when 'noop'
+        click(c, x)
+        c += 1
+        # ci = 1
+    when /addx (-?\d+)/
+        click(c, x)
+        c += 1
+        click(c, x)
+        c += 1
+        x += $1.to_i
+    end
+end
+
+p @sum
